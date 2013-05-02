35b913cdb2abe6e4390daf042b5aa5fe65814d79
Michael Hari <michaeldhari@gmail.com>
2013-05-01 20:34:57 -0700

    adding missing files

--- teamproj/MyInsurance/MyInsurance/Policy.h
+++ teamproj/MyInsurance/MyInsurance/Policy.h
@@ -8,9 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
-@interface Policy : NSObject
+@interface Policy : NSObject<NSXMLParserDelegate>
 
 @property(strong,nonatomic) NSMutableArray *drivers;
 @property(strong,nonatomic) NSMutableArray *vehicles;
+
+//<driver>
+//<address1>123 Fake Street</address1>
+//<city>San Leandro</city>
+//<dob>12/12/2013</dob>
+//<full_name>Michael David Hari</full_name>
+//<license_id>D2345151</license_id>
+//<state>CA</state>
+//<zipcode>94579</zipcode>
+//</driver>
+
+//@property(nonatomic) BOOL
+
+-(void)getDataFromWebService;
 
 @end
--- teamproj/MyInsurance/MyInsurance/Policy.m
+++ teamproj/MyInsurance/MyInsurance/Policy.m
@@ -7,6 +7,7 @@
 //
 
 #import "Policy.h"
+#import "Properties.h"
 
 @implementation Policy
 
@@ -19,6 +20,58 @@
     }
     
     return self;
+}
+
+-(void) displayAlert:(NSString *)_message{
+    
+    UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"Information" message:_message delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    [alertView show];
+}
+
+//-(void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *) namespaceURI qualifiedName:(NSString *)qName
+//   attributes: (NSDictionary *)attributeDict{
+//    if ([elementName isEqualToString:@"policy_number"]) {
+//        self.policyNumElementFound = YES;
+//    }
+//    
+//}
+//
+//- (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string{
+//    
+//    if (self.policyNumElementFound) {
+//        NSLog(@"policy_num:%@",string);
+//        NSUserDefaults *nsUserDefaults = [NSUserDefaults standardUserDefaults];
+//        [nsUserDefaults setValue:string forKey:@"policyNumber"];
+//        [nsUserDefaults synchronize];
+//        self.policyNumElementFound = NO;
+//    }
+//}
+
+-(void)getDataFromWebService{
+    
+    
+    NSString *webserviceURL = [NSString stringWithFormat:@"%@/policy?policyNumber=%@",BASE_WEBSERVICE_URL,[[NSUserDefaults standardUserDefaults] valueForKey:@"policyNumber"]];
+    
+    NSURL *url=[[NSURL alloc]initWithString:webserviceURL];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    
+    [request setValue:@"application/xml" forHTTPHeaderField:@"Content-Type"];
+    
+    NSHTTPURLResponse* urlResponse = nil;
+    NSError *error = [[NSError alloc] init];
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&urlResponse error:&error];
+    NSLog(@"urlResponse:%d",urlResponse.statusCode);
+    if(urlResponse.statusCode!=200){
+        [self displayAlert:@"Error from authentication server"];
+    }else{//success
+        NSXMLParser *xmlParser = [[NSXMLParser alloc] initWithData:data];
+        xmlParser.delegate=self;
+        [xmlParser parse];
+    }
+    
+    
+    
+    
 }
 
 @end
